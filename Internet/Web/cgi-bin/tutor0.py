9d9ca28260e22da6707bf74be2d6c5dcd8573470
wenhanglei <3188744608@qq.com>
2017-07-08 14:49:19 +0800

    添加第一个python中的cgi脚本

---
+++ Internet/Web/cgi-bin/tutor0.py
@@ -0,0 +1,9 @@
+"""
+在服务端运行，使用print函数创建新页面；
+url地址为：//localhost/cig-bin/turor0.py
+"""
+
+print('Contnet-type: text/html\n')
+print('<TITLE>CGI 101</TITLE>')
+print('<H1>A First CGI Script</H1>')
+print('<P>Hello, CGI World!</P>')
