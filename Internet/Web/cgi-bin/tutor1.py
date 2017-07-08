159d05f82571d28c8562cb1421dce57b517d8e9d
wenhanglei <3188744608@qq.com>
2017-07-08 15:38:56 +0800

    添加第二个cgi脚本，打开图片

---
+++ Internet/Web/cgi-bin/tutor1.py
@@ -0,0 +1,11 @@
+text = """Content-type: text/html
+
+<TITLE>CGI 101</TITLE>
+<H1>A Second CGI Script</H1>
+<HR>
+<P>Hello, CGI World!</p>
+<IMG src="../ppsmall.png" BORDER=1 ALT=[image]>
+<HR>
+"""
+
+print(text)
