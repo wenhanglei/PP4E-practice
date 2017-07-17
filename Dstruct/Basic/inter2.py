9a53da0900c5fa4f68943903fba2be75b4600d8a
wenhanglei <3188744608@qq.com>
2017-07-17 16:37:05 +0800

    优化~

---
+++ Dstruct/Basic/inter2.py
@@ -0,0 +1,48 @@
+"set operations for multiple sequences"
+
+def intersect(*args):
+    res = []
+    for x in args[0]:
+        for other in args[1:]:
+            if x not in other: break
+        else:
+            res.append(x)
+    return res
+
+def union(*args):
+    res = []
+    for seq in args:
+        for x in seq:
+            if not x in res:
+                res.append(x)
+    return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
