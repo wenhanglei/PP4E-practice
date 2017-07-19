cd25b5aeb533fbcd9e34505bd0436b5595327a6c
wenhanglei <3188744608@qq.com>
2017-07-19 14:25:12 +0800

    dictionary实现的图最短路劲查找

---
+++ Dstruct/Classics/gtestfunc.py
@@ -0,0 +1,35 @@
+"dictionary based graph representation"
+
+Graph = {'A': ['B', 'E', 'G'],
+         'B': ['C'],
+         'C': ['D', 'E'],
+         'D': ['F'],
+         'E': ['C', 'F', 'G'],
+         'F': [ ],
+         'G': ['A']}
+
+def tests(searcher):
+    print(searcher('E', 'D', Graph))
+    for x in ['AG', 'GF', 'BA', 'DA']:
+        print(x, searcher(x[0], x[1], Graph))
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
