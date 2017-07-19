bf3a85e1f96681e887b45cee6ab2e1f6c3913730
wenhanglei <3188744608@qq.com>
2017-07-19 14:33:16 +0800

    图查找实现（二）

---
+++ Dstruct/Classics/gsearch2.py
@@ -0,0 +1,49 @@
+"graph search, using paths stack instead of recursion"
+
+def search(start, goal, graph):
+    solns = generate(([start], []), goal, graph)
+    solns.sort(key=lambda x: len(x))
+    return solns
+def generate(paths, goal, graph):
+    solns = []
+    while paths:
+        front, paths = paths
+        state = front[-1]
+        if state == goal:
+            solns.append(front)
+        else:
+            for arc in graph[state]:
+                if arc not in front:
+                    paths = (front + [arc]), paths
+    return solns
+
+if __name__ == '__main__':
+    import gtestfunc
+    gtestfunc.tests(search)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
