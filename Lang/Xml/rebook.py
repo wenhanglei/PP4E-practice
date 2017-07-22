083a73c7efc13908e1dca63ccbe9070c0f8ea49b
wenhanglei <3188744608@qq.com>
2017-07-22 15:05:00 +0800

    使用re模块提取xml中的数据

---
+++ Lang/Xml/rebook.py
@@ -0,0 +1,24 @@
+"""
+XML parsing: regular expressions (no robust or general)
+"""
+import re, pprint
+text = open('books.xml').read()
+pattern = '(?s)isbn="(.*?)".*?<title>(.*?)</title>'
+found = re.findall(pattern, text)
+mapping = {isbn: title for (isbn, title) in found}
+pprint.pprint(mapping)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
